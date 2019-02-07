ce59aa47628a80e0b46f7f491fddcfc94b509046
Rikesh Puri <rikeshpuri@Rikeshs-MacBook-Pro.local>
2019-02-07 16:16:04 -0500

    gemfile update 3

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+	get 'bugs/index'
   resources :users
   resources :bugs
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
